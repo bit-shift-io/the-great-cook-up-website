61bd513a3e1d9830ae771e8f572fd4f4f4e70c22
supagu <supagu@users.noreply.github.com>
2025-03-25 21:46:27 +0000

    deploy: f9f3ca0dae4ad1913fc68545644659171f03316d

--- _next/static/chunks/app/[recipe]/page-06ce8b74721ece48.js
+++ _next/static/chunks/app/[recipe]/page-06ce8b74721ece48.js
@@ -1,4 +1,4 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[254],{1329:function(e,t,n){Promise.resolve().then(n.bind(n,2066))},2066:function(e,t,n){"use strict";n.r(t),n.d(t,{RecipeClient:function(){return F}});var r=n(7009),i=n(9512),l=n(9851),s=n.n(l),o=n(1627);/**
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[254],{1329:function(e,t,n){Promise.resolve().then(n.bind(n,9038))},9038:function(e,t,n){"use strict";n.r(t),n.d(t,{RecipeClient:function(){return F}});var r=n(7009),i=n(9512),l=n(9851),s=n.n(l),o=n(1627);/**
  * @license lucide-react v0.294.0 - ISC
  *
  * This source code is licensed under the ISC license.
